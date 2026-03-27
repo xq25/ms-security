f59e9d9f3e65bcc004b0602580d621ffd2a0dfd1
xq25 <jacoboq92@gmail.com>
2026-03-26 19:30:43 -0500

    Desarrollo que metodo para consultar perfil de usuario

--- src/main/java/backend/ms_security/Repositories/ProfileRepository.java
+++ src/main/java/backend/ms_security/Repositories/ProfileRepository.java
@@ -2,6 +2,10 @@ package backend.ms_security.Repositories;
 
 import backend.ms_security.Models.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ProfileRepository extends MongoRepository<Profile, String> {
+
+    @Query("{ 'user.$id' : ObjectId(?0) }")
+    public Profile findProfileByUserID(String user_id);
 }
